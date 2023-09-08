5d305eb4e8e0885f055de6625d25fa6a897e8d49
rhakagesa <rhakagesa@gmail.com>
2023-09-08 08:14:11 +0700

    Quiz Part 8.6 Done

--- part8.6/quiz4.js
+++ part8.6/quiz4.js
@@ -1,13 +1,14 @@
 function tentukanDeretGeometri(arr) {
   // you can only write your code here!
   let rasio1 = arr[arr.length - 1] / arr[arr.length - 2];
-  let decrement = arr.length;
+  let indexBelakang = arr.length - 1;
   for (let i = 0; i < arr.length; i++) {
-    if (i !== decrement) {
-      let rasio2 = arr[decrement - 1] / arr[decrement - 2];
+    if (i !== arr.length - 1) {
+      let rasio2 = arr[indexBelakang] / arr[indexBelakang - 1];
+
       if (rasio1 === rasio2) {
         rasio1 = rasio2;
-        decrement--;
+        indexBelakang--;
       } else {
         return false;
       }
@@ -23,3 +24,4 @@ console.log(tentukanDeretGeometri([2, 4, 8, 16, 32])); // true
 console.log(tentukanDeretGeometri([2, 4, 6, 8])); // false
 console.log(tentukanDeretGeometri([2, 6, 18, 54])); // true
 console.log(tentukanDeretGeometri([1, 2, 3, 4, 7, 9])); // false
+console.log(tentukanDeretGeometri([5, 25, 125, 625, 3125])); // true
